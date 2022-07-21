7f594aa930c4595fb56a50a76f7c4565ae765d92
Ethan Cho <ethan.cho1826@gmail.com>
2022-07-20 20:49:19 -0500

    added comments for syntax explanation

--- ballot.sol
+++ ballot.sol
@@ -1,8 +1,15 @@
+// specifies the version of solidity we are writing in/what we are compiling in
 pragma solidity ^0.4.17;
 
+// contract defines a new contract definition equivalent to classes in programming languages
 contract Inbox {
+    // storage variable, basically the same as an attribute on a class
+    // the storage variable will stay on the blockchain with the contract in contrast to a local variable which will not be stored onto the blockchain and "garbage collected"/thrown away
+    // public is the same as java, specifies who has access to this variable
     string public message;
 
+    // functions defined on the contract, like instance methods on a class
+    // if the function has the same name as the contract it is the constructor function or the init function
     function Inbox(string initialMessage) public {
         message = initialMessage;
     }
