dacab7f2b1febe8f1de6141777cda5f996b0d6f6
Ethan Cho <ethan.cho1826@gmail.com>
2022-07-21 17:52:55 -0500

    added comments for what the code is saying

--- compile.js
+++ compile.js
@@ -3,7 +3,10 @@ const fs = require('fs');
 const path = require('path');
 const solc = require('solc');
 
+// getting the folder path for our inbox contract
 const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+// actually unpackaging the source code of the contract
 const source = fs.readFileSync(inboxPath, 'utf8');
 
-console.log(solc.compile(source, 1));
+// exporting just the definition of the compiled contract for deployment
+module.exports = solc.compile(source, 1).contracts[':Inbox'];
