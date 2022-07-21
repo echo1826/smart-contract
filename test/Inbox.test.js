94c8ade0975e6d9276e54e38fa6af77859f390b3
Ethan Cho <ethan.cho1826@gmail.com>
2022-07-21 17:53:22 -0500

    created test that tests if the contract deploys onto the network

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,13 +1,32 @@
 // contract test code will go here
 const assert = require('assert');
 const ganache = require('ganache-cli');
-const { describe } = require('mocha');
 const Web3 = require('web3');
+const {interface, bytecode} = require('../compile');
 
 // creating a web3 instance passing in a provider that tells it what network we want to connect to
 const web3 = new Web3(ganache.provider());
+let fetchedAccounts;
+let inbox;
 
+beforeEach(async () => {
+    // get a list of all accounts
+    fetchedAccounts = await web3.eth.getAccounts();
+    // use one of those accounts to deploy the contract
+    // .Contract tells the web3 connection what the inbox contract is
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+    // deploy method is telling web3 to deploy a new copy of the contract
+    .deploy({data: bytecode, arguments: ['Hello World!']})
+    // send method tells web3 to send out a transaction that creates the contract
+    .send({from: fetchedAccounts[0], gas: '1000000'})
+});
 
+describe('Inbox',() => {
+    it('deploys a contract', () => {
+        // console.log(fetchedAccounts);
+        console.log(inbox);
+    });
+});
 
 // PRACTICE MOCHA SYNTAX
 // class Car {
