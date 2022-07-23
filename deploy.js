32cd0fb71912e74ec1e3d7eb079ba6b79446eb60
Ethan Cho <ethan.cho1826@gmail.com>
2022-07-23 11:14:47 -0500

    deployed contract to rinkeby

--- deploy.js
+++ deploy.js
@@ -6,10 +6,20 @@ const {interface, bytecode} = require('./compile');
 
 const api = `https://rinkeby.infura.io/v3/${process.env.API_KEY}`;
 
-
+const initialString = "Hello World!";
 const provider = new HDWalletProvider(
     process.env.MNEMONIC,
     api
 );
 const web3 = new Web3(provider);
 
+async function deploy() {
+    const accounts = await web3.eth.getAccounts();
+    console.log("Attempting to deploy from account", accounts[0]);
+
+    const result = await new web3.eth.Contract(JSON.parse(interface)).deploy({data: bytecode, arguments: [initialString]}).send({from: accounts[0], gas: '1000000'});
+
+    console.log("contract deployed to", result.options.address);
+}
+
+deploy();
