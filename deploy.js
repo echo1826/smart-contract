f01865e033e87f0528eb5a0a4a47bc5c89ecd4fc
Ethan Cho <ethan.cho1826@gmail.com>
2022-07-23 10:50:44 -0500

    connected web3 to rinkeby

--- deploy.js
+++ deploy.js
@@ -1,1 +1,15 @@
 // deploy code will go here
+require('dotenv').config();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const {interface, bytecode} = require('./compile');
+
+const api = `https://rinkeby.infura.io/v3/${process.env.API_KEY}`;
+
+
+const provider = new HDWalletProvider(
+    process.env.MNEMONIC,
+    api
+);
+const web3 = new Web3(provider);
+
