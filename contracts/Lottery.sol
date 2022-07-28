e6ed4278d476310e5bbb6aa604cb8dd8fba3f9ae
Ethan Cho <ethan.cho1826@gmail.com>
2022-07-28 15:06:40 -0500

    enter lottery function

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -14,4 +14,12 @@ contract Lottery {
         // contains information about the address that started the current function invocation, data field from the call or transaction that invoked the current function (data), amount of gas the current function invocation has available (gas), amount of ether, in wei, that was sent along with the function invocation
         manager = msg.sender;
     }
+
+    // payable identifier means that there will be ether attached whenever this function is invoked/called
+    function enter() public payable {
+        // require is a global function that can be accessed anywhere in solidity, it's used for validation, like validating the player paid enough ether to enter into the lottery
+        require(msg.value > .01 ether);
+
+        players.push(msg.sender);
+    }
 }
